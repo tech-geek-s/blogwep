53a97d05eeacb19ac9f2dec7b345faed4e5b77bb
fida-ur-rehman <55063076+fida-ur-rehman@users.noreply.github.com>
2020-05-19 18:10:57 +0530

    cookies|session & login btn

--- index.js
+++ index.js
@@ -1,75 +1,142 @@
 
 
 const express=require("express");
-const bodyparser=require('body-parser');
+const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const mongoose=require('mongoose');
 const request=require('request');
-
+const session=require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 const app=express();
 
-app.set('view engine', 'ejs')
-app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/blogwebdb",{useNewUrlParser:true});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(session({
+  secret: "our littel secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/blogwebdb",{useUnifiedTopology: true, useNewUrlParser:true});
+mongoose.set("useCreateIndex", true);
 
 const inc= "";
 var likes=0;
+let clientStatus = ""
+let logButton = ""
 
-const clientinfo={
-  email:String,
-  password:String
-};
-const blogdata={
+const clientSchema= new mongoose.Schema({
+  email: String,
+  password: String
+});
+const blogdataSchema=new mongoose.Schema({
   title:String,
   content:String
-}
-
-const answersSchema = {
+});
+const answersSchema = new mongoose.Schema({
   description: String,
   code: String
-}
+});
 
-const QuestionsSchema={
+const QuestionsSchema=new mongoose.Schema({
   title: String,
   description: String,
   code: String,
   answers: [answersSchema]
-}
+});
 
-const Client = mongoose.model("Client",clientinfo)
-const BlogData=mongoose.model("BlogData",blogdata)
-const Answer = mongoose.model("Answer", answersSchema)
-const Question = mongoose.model("Question",QuestionsSchema);
+clientSchema.plugin(passportLocalMongoose);
 
+const Client = new mongoose.model("Client",clientSchema);
+const BlogData= new mongoose.model("BlogData",blogdataSchema);
+const Answer = new mongoose.model("Answer", answersSchema);
+const Question =new mongoose.model("Question",QuestionsSchema);
+
+passport.use(Client.createStrategy());
+
+passport.serializeUser(function(client, done) {
+  done(null, client.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Client.findById(id, function(err, client) {
+    done(err, client);
+  });
+});
 
 app.get("/",function(req,res){
-  res.render("home")
+  if(req.isAuthenticated()){
+    clientStatus = "/logout"
+    logButton = "Logout"
+    res.render("home", {clientStatus: clientStatus, logButton: logButton});
+} else {
+  clientStatus = "/signup"
+  logButton = "signup"
+    res.render("home", {clientStatus: clientStatus, logButton: logButton});
+}
 })
 
 
 app.get("/signup",function(req,res){
-  res.render("signup")
+  clientStatus = "/signin"
+  logButton = "SignIn"
+  res.render("signup", {clientStatus: clientStatus, logButton: logButton})
 })
 
 
 app.get("/signin",function(req,res){
-  res.render("signin",{var1:inc})
+  clientStatus = "/signup"
+  logButton = "SignUp"
+  res.render("signin",{var1:inc, clientStatus: clientStatus, logButton: logButton})
 })
 
 
 app.get("/questions", function(req,res){
+
+  if(req.isAuthenticated()){
+    clientStatus = "/logout"
+    logButton = "Logout"
+    Question.find(function(err, foundQuestions){
+      if(!err){
+        res.render("questions", {questions: foundQuestions, clientStatus: clientStatus, logButton: logButton});
+      } else {
+        res.send(err);
+      }
+    })
+   
+} else {
+  clientStatus = "/signin"
+  logButton = "SignIn"
   Question.find(function(err, foundQuestions){
     if(!err){
-      res.render("questions",{questions: foundQuestions});
+      res.render("questions", {questions: foundQuestions, clientStatus: clientStatus, logButton: logButton});
     } else {
       res.send(err);
     }
   })
+    
+}
 })
 
 app.get("/ask_question", function(req,res){
-  res.render("ask_question");
+  if(req.isAuthenticated()){
+    clientStatus = "/logout"
+    logButton = "Logout"
+    res.render("ask_question", {clientStatus: clientStatus, logButton: logButton});
+} else {
+  clientStatus = "/signin"
+  logButton = "SignIn"
+    res.render("signin", {var1:inc,clientStatus: clientStatus, logButton: logButton});
+}
+ 
 })
 
 app.post("/ask_question", function (req, res) {
@@ -92,23 +159,39 @@ res.redirect("/questions");
 
 app.get("/blog",function(req,res){
 
-  BlogData.find({},function(err,foundPost){
-          res.render("blog",{likes:likes, title:foundPost.title, content:foundPost.content, BlogData: foundPost})
-  })
+  if(req.isAuthenticated()){
+    clientStatus = "logout"
+    logButton = "Logout"
+    BlogData.find({},function(err,foundPost){
+      res.render("blog",{likes:likes, title:foundPost.title, content:foundPost.content, BlogData: foundPost, clientStatus: clientStatus, logButton: logButton})
+})
 
+} else {
+  clientStatus = "login"
+  logButton = "Login"
+  BlogData.find({},function(err,foundPost){
+    res.render("blog",{likes:likes, title:foundPost.title, content:foundPost.content, BlogData: foundPost, clientStatus: clientStatus, logButton: logButton})
+})
+
+}
 })
 app.post("/blog",function(req,res){
-
- likes=likes+1
-
- BlogData.find({},function(err,foundPost){
-  res.render("blog",{likes:likes, title:foundPost.title, content:foundPost.content, BlogData: foundPost})
+  if(req.isAuthenticated()){
+     likes=likes+1
+     BlogData.find({},function(err,foundPost){
+     res.render("blog",{likes:likes, title:foundPost.title, content:foundPost.content, BlogData: foundPost})
+    })
+  } else {
+    res.redirect("/signin");
+  }
 })
-})
-
 
 app.get("/blogsubmit",function(req,res){
-  res.render("blogsubmit")
+  if(req.isAuthenticated()){
+    res.render("blogsubmit");
+} else {
+    res.redirect("/login");
+}
 })
 
 
@@ -126,62 +209,78 @@ app.post("/blogsubmit",function(req,res){
       console.log(err)
     }
   })
-        res.redirect("/blog")
+        res.redirect("blog")
 
 })
 
 
 
 app.post("/signup",function(req,res){
-const  newclient = new Client({
-  email:req.body.eml,
-  password:req.body.pass1
-});
-newclient.save(function(err){
-  if(!err){
-    console.log("successfully inserted");
+  Client.register({username :req.body.username}, req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+    res.redirect("signup");
   }else{
-    console.log(err);
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("blog");
+    })
   }
 })
-
 });
 
 
 
 app.post("/signin",function(req,res){
+  const client = new Client({
+    email: req.body.username,
+    password: req.body.password
+})
 
-  const username=req.body.email;
-  const password=req.body.password;
-  Client.findOne({email:username},function(err,findclient){
-    if(!err){
-      if(findclient.password==password){
-        console.log("found");
-        res.render("home");
-      }
-      else{
-        inc="notfound";
-      res.render("signin",{var1:inc});
-      }
+req.login(client, function(err){
+    if(err){
+        console.log(err);
+    }else {
+        passport.authenticate("local")(req, res,function(){
+            res.redirect("blog");
+        })
     }
-
-    else{
-    console.log(err)
-  }
-  })
+})
 });
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+})
 
 
 
 app.get("/:questionTitle", function(req,res){
+
   const questionTitle = req.params.questionTitle;
+
+  if(req.isAuthenticated()){
+    clientStatus = "/logout"
+    logButton = "Logout"
+    Question.findOne({title: questionTitle}, function(err, foundQuestion){
+      if(foundQuestion){
+        res.render("singleQuestion",{_id: foundQuestion._id, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers, clientStatus: clientStatus, logButton: logButton})
+      } else {
+        console.log(err);
+      }
+    })
+} else {
+  clientStatus = "/signup"
+  logButton = "signup"
   Question.findOne({title: questionTitle}, function(err, foundQuestion){
     if(foundQuestion){
-      res.render("singleQuestion",{_id: foundQuestion._id, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers})
+      res.render("singleQuestion",{_id: foundQuestion._id, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers, clientStatus: clientStatus, logButton: logButton})
     } else {
       console.log(err);
     }
   })
+}
+
+ 
 })
 
 app.post("/:questionId", function(req, res){
@@ -191,17 +290,34 @@ app.post("/:questionId", function(req, res){
     description: req.body.answerDescription,
     code: req.body.answerCode
   })
-  
+
+  if(req.isAuthenticated()){
+    clientStatus = "/logout"
+    logButton = "Logout"
+    Question.findOne({_id: questionId}, function(err, foundQuestion){
+      if(foundQuestion){
+        foundQuestion.answers.push(newAnswer);
+        foundQuestion.save();
+        res.render("singleQuestion",{_id: questionId, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers, clientStatus: clientStatus, logButton: logButton})
+      } else {
+        res.send(err);
+      }
+    }) 
+
+} else {
+  clientStatus = "/signup"
+  logButton = "signup"
   Question.findOne({_id: questionId}, function(err, foundQuestion){
     if(foundQuestion){
       foundQuestion.answers.push(newAnswer);
       foundQuestion.save();
-      res.render("singleQuestion",{_id: questionId, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers})
+      res.render("singleQuestion",{_id: questionId, title: foundQuestion.title, description: foundQuestion.description, code: foundQuestion.code, answers: foundQuestion.answers, clientStatus: clientStatus, logButton: logButton})
     } else {
       res.send(err);
     }
-  })   
-  })
+  }) 
+}   
+})
 
 app.listen(3000,function(){
   console.log("server started")
